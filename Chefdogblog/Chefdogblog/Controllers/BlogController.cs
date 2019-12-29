06df60f5b942be474b077ccd1f0f9e022c0d3044
Marco van Zuijlen <marco@chefdog.com>
2019-12-29 10:37:01 +0100

    major refresh, medium style like theme and added file upload for preview img

---
+++ Chefdogblog/Chefdogblog/Controllers/BlogController.cs
@@ -0,0 +1,37 @@
+﻿using Chefdogblog.Intefaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System;
+using System.Threading.Tasks;
+
+namespace Chefdogblog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        private IBlogService blogService;
+        private IOptionsSnapshot<BlogSettings> blogSettings;
+
+        public BlogController(IBlogService blog, IOptionsSnapshot<BlogSettings> settings)
+        {
+            blogService = blog;
+            blogSettings = settings;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetPosts(int page = 0) {
+            try
+            {
+                var posts = await blogService.GetPosts(blogSettings.Value.PostsPerPage, blogSettings.Value.PostsPerPage * page);
+                return Ok(posts);
+            }
+            catch (Exception ex) {
+                return NotFound();
+            }
+        }
+    }
+}
