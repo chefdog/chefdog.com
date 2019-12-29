aa4ad474db9cbaed288b9097d7b2c7fff8d328ee
Marco van Zuijlen <marco@chefdog.com>
2019-12-29 12:58:36 +0100

    Added styling, added sub features

--- Miniblog/BlogSettings.cs
+++ Miniblog/BlogSettings.cs
@@ -2,10 +2,12 @@
 {
     public class BlogSettings
     {
-        public string Name { get; set; } = "Miniblog";
-        public string Description { get; set; } = "A short description of the blog";
-        public string Owner { get; set; } = "The Owner";
+        public string Name { get; set; } = "Coffee blog";
+        public string Description { get; set; } = "Coffee reviews and taste sessions";
+        public string Owner { get; set; } = "Chefdog";
         public int PostsPerPage { get; set; } = 2;
         public int CommentsCloseAfterDays { get; set; } = 10;
+        public string DefaultFeaturedPreviewImageUrl { get; set; } = "/img/featuredDefault.jpg";
+        public string DefaultPreviewImageUrl { get; set; } = "/img/postDefault.jpg";
     }
 }
