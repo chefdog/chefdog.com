11b070ef85ecc7f3d7391c838cc21b7f2e2e4cc7
Marco van Zuijlen <marco@chefdog.com>
2021-10-31 09:59:44 +0100

    next version with next schema

---
+++ cfg-cms/seed-data/data.ts
@@ -0,0 +1,4 @@
+export const authors = [
+  { name: 'Marco van Zuijlen', email: 'marco@chefdog.com', password: '1B@wie23' },
+  { name: 'Stefan Thie', email: 'stefan@chefdog.com', password: '1B@wie23' },
+];
---
+++ cfg-cms/seed-data/index.ts
@@ -0,0 +1,37 @@
+import { KeystoneContext } from '@keystone-next/keystone/types';
+import { authors } from './data';
+
+type AuthorProps = {
+    name: string;
+    email: string;
+  };
+
+export async function insertSeedData(context: KeystoneContext) {
+    console.log(`🌱 Inserting seed data`);
+
+    const createAuthor = async (authorData: AuthorProps) => {
+        let author = null;
+        try {
+          author = await context.query.User.findOne({
+            where: { email: authorData.email },
+            query: 'id',
+          });
+        } catch (e) {}
+        if (!author) {
+          author = await context.query.User.createOne({
+            data: authorData,
+            query: 'id',
+          });
+        }
+        return author;
+    };
+
+    for (const author of authors) {
+        console.log(`👩 Adding author: ${author.name}`);
+        await createAuthor(author);
+    }
+
+    console.log(`✅ Seed data inserted`);
+    console.log(`👋 Please start the process with \`yarn dev\` or \`npm run dev\``);
+    process.exit();
+}
