0e11daea737e5e16a33ed0cc6e58da7595c78599
Marco van Zuijlen <marco@chefdog.com>
2022-03-14 08:58:40 +0100

    environment

--- cfg-next/lib/apollo-client.tsx
+++ cfg-next/lib/apollo-client.tsx
@@ -1,6 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const Client = new ApolloClient({
+    // uri: process.env.API,
     uri: 'http://localhost:3000/api/graphql',
     cache: new InMemoryCache()
 });
