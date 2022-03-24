f6f586623732bc6e79a3d1b1d07f2afed6290126
Marco van Zuijlen <marco@chefdog.com>
2022-03-24 17:24:26 +0100

    working google maps

--- cfg-next/pages/search/index.tsx
+++ cfg-next/pages/search/index.tsx
@@ -1,72 +1,73 @@
+import { useEffect, useState } from "react";
 import Container from "../../components/container"
 import Header from "../../components/header"
 import Layout from "../../components/layout"
 
+import Map from '../../lib/map';
+import { loadMapApi } from "../../lib/google-maps-util";
+import Footer from "../../components/footer";
+
 const Search  = () => { 
-    return (
-    
+
+	const [scriptLoaded, setScriptLoaded] = useState(false);
+    const [distanceInKm, setDistanceInKm] = useState<number>(-1);
+
+    useEffect(() => {
+        const googleMapScript = loadMapApi();
+        googleMapScript.addEventListener('load', function () {
+            setScriptLoaded(true);
+        });
+    }, []);
+
+    const renderDistanceSentence = () => {
+        return (
+            <div className='distance-info'>
+                {`Distance between selected marker and home address is ${distanceInKm}km.`}
+            </div>
+        );
+    };
+
+    return (    
         <Layout>
             <Container>
                 <Header></Header>
 
-                <section id="service-section" className="service-section sec-ptb-100 text-center clearfix">
-				<div className="container">
+				<section id="service-section" className="service-section sec-ptb-100 text-center clearfix">
+					<div className="container">
 
-					<div className="row justify-content-center">
-						<div className="col-lg-7 col-md-8 col-sm-9 aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
-							<div className="section-title size-increase">
-								<h2 className="title-text mb-0">
-									After coming into contact with a religious man I always feel I must wash my hands.
-								</h2>
-							</div>
+						<div className="row justify-content-center">
+
+							<form id="contact_form" action="mail.php" method="POST">
+								<div className="row justify-content-center aos-init aos-animate" data-aos="fade-up" data-aos-delay="300">
+									<div className="col-lg-12">
+										<div className="form-item">
+											<input type="text" name="name" placeholder="Search on..." />
+										</div>
+									</div>
+
+								</div>
+								<div className="text-center aos-init aos-animate" data-aos="fade-up" data-aos-delay="500">
+									<button type="submit" className="btn" value="submit">Search</button>
+								</div>
+							</form>
+
 						</div>
 					</div>
+				</section>
 
-					<div className="row">
-						<div className="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-							<div className="service-default plr-30 aos-init aos-animate" data-aos="fade-up" data-aos-delay="300">
-								<span className="item-icon">
-									<img src="assets/images/icons/icon_14.png" alt="image_not_found" />
-								</span>
-								<h3 className="item-title">Head Quarter</h3>
-								<p className="mb-0">
-									Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine.
-								</p>
-							</div>
-						</div>
-
-						<div className="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-							<div className="service-default plr-30 aos-init aos-animate" data-aos="fade-up" data-aos-delay="500">
-								<span className="item-icon">
-									<img src="assets/images/icons/icon_15.png" alt="image_not_found" />
-								</span>
-								<h3 className="item-title">Phone Numbers</h3>
-								<p className="mb-0">
-									Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. 
-								</p>
-							</div>
-						</div>
-
-						<div className="col-lg-4 col-md-4 col-sm-12 col-xs-12">
-							<div className="service-default plr-30 aos-init aos-animate" data-aos="fade-up" data-aos-delay="700">
-								<span className="item-icon">
-									<img src="assets/images/icons/icon_16.png" alt="image_not_found" />
-								</span>
-								<h3 className="item-title">Emails &amp; Support</h3>
-								<p className="mb-0">
-									Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. 
-								</p>
-							</div>
-						</div>
-					</div>
-
-				</div>
-			</section>
-
-                <section id="map-section" className="map-section clearfix">
-                    
+				<section id="map-section" className="map-section clearfix">
+					{scriptLoaded && (
+						<Map
+							mapType={google.maps.MapTypeId.ROADMAP}
+							mapTypeControl={true}
+							setDistanceInKm={setDistanceInKm}
+						/>
+					)}
+					{distanceInKm > -1 && renderDistanceSentence()}
                 </section>
             </Container>
+
+			<Footer></Footer>
         </Layout>
     )
 }
