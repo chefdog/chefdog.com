21592c738c94a058f26fdbbd73d8e2f766539cd6
Marco van Zuijlen <marco@chefdog.com>
2022-05-26 17:27:03 +0200

    oops

--- chefdog.frontend/lib/google-maps-util.ts
+++ chefdog.frontend/lib/google-maps-util.ts
@@ -1,6 +1,6 @@
 
 export const loadMapApi = () => {
-    const mapsURL = `https://maps.googleapis.com/maps/api/js?key=AIzaSyCWX1Q3UhG8Z764VQKATjXIvrqYk6JNLhQ&libraries=geometry,places&language=nl&region=NL&v=quarterly`;
+    const mapsURL = ``;
     const scripts = document.getElementsByTagName('script');
     // Go through existing script tags, and return google maps api tag when found.
     for (let i = 0; i < scripts.length; i++) {
